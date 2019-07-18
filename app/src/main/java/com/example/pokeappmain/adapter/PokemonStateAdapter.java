5d0501605c7535494ff426b60cbdae123c5e1d64
Neyasbit <kobzevjob@gmail.com>
2019-07-18 09:45:44 +0700

    Created PokemonStateAdapter

--- app/src/main/java/com/example/pokeappmain/adapter/PokemonStateAdapter.java
+++ app/src/main/java/com/example/pokeappmain/adapter/PokemonStateAdapter.java
@@ -1,9 +1,12 @@
 package com.example.pokeappmain.adapter;
 
+
 import android.content.Context;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
